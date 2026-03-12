2a7b8d25967269c117764f51375c1ad8cbff3fbc
Semyon52 <semaprogo@gmail.com>
2026-03-12 12:21:39 +0500

    Delete 2  Задание тип 2.py

--- 2  Задание тип 2.py
+++
@@ -1,6 +0,0 @@
-for w in range(2):
-    for x in range(2):
-        for y in range(2):
-            for z in range(2):
-                if not((x == (not y)) <= ((z<=(not w)) and (w <= y))):
-                    print(w, x, y, z)
