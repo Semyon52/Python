942d882ef6007df37c870ff936a1e9543117b6c9
Semyon52 <semaprogo@gmail.com>
2026-03-12 12:22:03 +0500

    Delete 3 задание 2 тип.py

--- 3 задание 2 тип.py
+++
@@ -1,6 +0,0 @@
-for w in range(2):
-    for x in range(2):
-        for y in range(2):
-            for z in range(2):
-                if (((x<=y)or (z<=w)) and ((z==y)<=(w==x)))==0:
-                    print(w, x, y, z)
